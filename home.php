4539779a826de7eb6d190ea592dedf50de5dd24c
SarthakKeshari <55992140+SarthakKeshari@users.noreply.github.com>
2021-11-08 01:01:18 +0530

    Changed bootstrap stylesheet from http to https

--- home.php
+++ home.php
@@ -40,7 +40,7 @@
                     data-bs-dismiss="offcanvas" aria-label="Close"></button> -->
                 <a href="./index.php" style="text-decoration: none;">
                     <button class="btn btn-primary text-white text-right d-flex align-items-center" style="background-color: #005397; color: #005397; box-shadow: 0px 1px 1px 1px #00539740;">
-                        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-power" viewBox="0 0 16 16">
+                        <svg xmlns="https://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-power" viewBox="0 0 16 16">
                             <path d="M7.5 1v7h1V1h-1z"/>
                             <path d="M3 8.812a4.999 4.999 0 0 1 2.578-4.375l-.485-.874A6 6 0 1 0 11 3.616l-.501.865A5 5 0 1 1 3 8.812z"/>
                         </svg> &nbsp;
